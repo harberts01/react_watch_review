f071b06ee891ec88159d14f22ae1a412e0b701c2
harberts01 <109708014+harberts01@users.noreply.github.com>
2022-11-04 13:29:28 -0500

    Committing React Project Files

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './App.css';
+import './lib/font-awesome/css/all.min.css'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Header } from './components/header';
+import { Watched } from './components/Watched'
+import { Watchlist }  from './components/watchlist';
+import {Add} from './components/Add';
+// import {FiveStars} from './components/five-stars';
+import { GlobalProvider } from './context/GlobalState';
+
+
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Router>
+        <Header/>
+
+          <Routes>
+
+              <Route exact path="/" element={<Watchlist/>}>
+                
+              </Route>
+
+              <Route path="/Watched" element = {<Watched/>}>
+                
+              </Route>
+
+              <Route path="/Add" element={<Add/>}>
+                
+              </Route>
+
+          </Routes>
+
+      </Router>
+    </GlobalProvider>
+  ); 
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+export const Header = () => {
+  return (
+    <header>
+        <div className='container'>
+            <div className='inner-content'>
+                <div className='brand'>
+                    <Link to='/'>Watch Review</Link>
+                </div>
+
+                <ul className='nav-links'>
+                    <li>
+                        <Link to="/">Watch List</Link>
+                    </li>
+                    <li>
+                        <Link to="/Watched">Watched</Link>
+                    </li>
+                    <li>
+                        <Link to="/Add" className='btn btn-danger'>Add</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
+)
+}
